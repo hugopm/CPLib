0edfc999ef5fa0bd8e06ef108feb42dd5866b836
Hugo <hugo.pm@protonmail.com>
2020-12-24 18:38:11 +0100

    Add Rafbill debug

--- core.hpp
+++ core.hpp
@@ -22,22 +22,13 @@ int len(const T &x) {
 }
 
 // DEBUG
-int recur_depth = 0;
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 #ifdef DEBUG
-#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
-#define dbg(x)
+#define dbg(...)
 #endif
-template<typename Ostream, typename Cont>
-typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
-	os<<"[";
-	for(auto& x:v){os<<x<<", ";}
-	return os<<"]";
-}
-template<typename Ostream, typename ...Ts>
-Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
-	return os<<"{"<<p.first<<", "<<p.second<<"}";
-}
 // END DEBUG
 
 template<int D, typename T>
---
+++ debug.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include "hl/core.hpp"
+
+template<class T, size_t... I>
+void print_tuple(ostream& s, T const& a, index_sequence<I...>){
+  using swallow = int[];
+  (void)swallow{0, (void(s << (I == 0? "" : ", ") << get<I>(a)), 0)...};
+}
+ 
+template<class T>
+ostream& print_collection(ostream& s, T const& a);
+template<class... A>
+ostream& operator<<(ostream& s, tuple<A...> const& a);
+template<class A, class B>
+ostream& operator<<(ostream& s, pair<A, B> const& a);
+ 
+template<class T, size_t I>
+ostream& operator<<(ostream& s, array<T, I> const& a) { return print_collection(s, a); }
+template<class T>
+ostream& operator<<(ostream& s, vector<T> const& a) { return print_collection(s, a); }
+template<class T>
+ostream& operator<<(ostream& s, deque<T> const& a) { return print_collection(s, a); }
+template<class T, class U>
+ostream& operator<<(ostream& s, multimap<T, U> const& a) { return print_collection(s, a); }
+template<class T>
+ostream& operator<<(ostream& s, multiset<T> const& a) { return print_collection(s, a); }
+template<class T, class U>
+ostream& operator<<(ostream& s, map<T, U> const& a) { return print_collection(s, a); }
+template<class T>
+ostream& operator<<(ostream& s, set<T> const& a) { return print_collection(s, a); }
+ 
+template<class T>
+ostream& print_collection(ostream& s, T const& a){
+  s << '[';
+  for(auto it = begin(a); it != end(a); ++it){
+    s << *it;
+    if(it != prev(end(a))) s << " ";
+  }
+  return s << ']';
+}
+ 
+template<class... A>
+ostream& operator<<(ostream& s, tuple<A...> const& a){
+  s << '(';
+  print_tuple(s, a, index_sequence_for<A...>{});
+  return s << ')';
+}
+ 
+template<class A, class B>
+ostream& operator<<(ostream& s, pair<A, B> const& a){
+  return s << "(" << get<0>(a) << ", " << get<1>(a) << ")";
+}
+ 
+ 
+static inline void debug_impl_seq() {
+  cerr << "}";
+}
+ 
+template <class T, class... V>
+void debug_impl_seq(T const& t, V const&... v) {
+  cerr << t;
+  if(sizeof...(v)) { cerr << ", "; }
+  debug_impl_seq(v...);
+}
