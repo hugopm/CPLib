549f32b0e6a2c76f16e935e63c8c0b4d980d701a
Hugo <hugo.pm@protonmail.com>
2020-12-26 17:39:02 +0100

    Upd core

--- core.hpp
+++ core.hpp
@@ -1,12 +1,16 @@
 #pragma once
 #include <bits/stdc++.h>
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define rep(i, a, b) for(int i = (a); i < (b); i++)
 using namespace std;
+
+// Utilities
+
 using ll = long long;
 const int m197 = 1000000007;
 const int m998 = 998244353;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
 
 template<typename T>
 void chmax(T &x, const T &v) {
@@ -27,9 +31,12 @@ int len(const T &x) {
 	return (int)(x.size());
 }
 
+// Debug
+
 void dbg_out() {
 	cout << endl;
 }
+
 template<typename Head, typename... Tail>
 void dbg_out(Head H, Tail... T) {
 	cout << ' ' << H;
@@ -57,6 +64,8 @@ Ostream& operator<<(Ostream& os,  const pair<Ts...>& p) {
 	return os << "{" << p.first << ", " << p.second << "}";
 }
 
+// Multi-dimensional vector
+
 template<int D, typename T>
 struct Vec : public vector<Vec<D - 1, T>> {
 	static_assert(D >= 1, "Vector dimension must be greater than zero!");
@@ -68,6 +77,8 @@ template<typename T>
 struct Vec<1, T> : public vector<T> {
 	Vec(int n, const T& val = T()) : vector<T>(n, val) {}
 };
+
+// Recursive lambda
 
 template<class Fun>
 class letrec_result {
@@ -87,24 +98,30 @@ decltype(auto) letrec(Fun &&fun) {
 	return letrec_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
 }
 
+// Input / Output
+
 ll nxt() {
 	ll x;
 	cin >> x;
 	return x;
 }
 
-vector<int> vec32(int n) {
-	vector<int> v(n);
-	for (int &x : v) {
+template<typename T>
+vector<T> read_vector(int n) {
+	vector<T> v(n);
+	for (T &x : v) {
 		cin >> x;
 	}
 	return v;
 }
 
-vector<ll> vec64(int n) {
-	vector<ll> v(n);
-	for (ll &x : v) {
-		cin >> x;
+template<typename T>
+void print_vector(vector<T> data, bool new_line, bool print_size) {
+	int n = data.size();
+	if (print_size) {
+		cout << n << '\n';
 	}
-	return v;
+	for (int i = 0; i < n; ++i) {
+		cout << data[i] << " \n"[i+1 == n || new_line];
+	}
 }
